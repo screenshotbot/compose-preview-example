99ba9cf61451dc6a9c66951537c35d344fe762c4
Arnold Noronha <arnold@tdrhq.com>
2024-05-24 13:41:56 -0400

    Add screenshotbot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.screenshot)
+    id("io.screenshotbot.plugin") version "1.13"
 }
 
 android {
